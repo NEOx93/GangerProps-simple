17dec753cb3b36e7f5d3cd0d615efe2cfab3890d
NEOx93 <33138168+NEOx93@users.noreply.github.com>
2025-11-13 06:48:35 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/data", express.static(path.join(__dirname, "data")));
+
+app.listen(PORT, ()=> console.log("Running on", PORT));
