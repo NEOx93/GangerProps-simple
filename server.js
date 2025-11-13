eb5759fa6c79cdbe3b48bf8c93c86fd00f2d59a0
NEOx93 <33138168+NEOx93@users.noreply.github.com>
2025-11-13 07:20:56 -0800

    Add root route to serve index.html

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from /public
 app.use(express.static(path.join(__dirname, "public")));
+
+// Serve data folder
 app.use("/data", express.static(path.join(__dirname, "data")));
 
-app.listen(PORT, ()=> console.log("Running on", PORT));
+// Root route -> send index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log("GangerProps.ai running on port", PORT);
+});
